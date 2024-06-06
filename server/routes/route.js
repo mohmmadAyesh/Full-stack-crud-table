a278878260b75f330927e4d980a6a81edfd4c01c
mohmmad ayesh <73184801+mohmmadAyesh@users.noreply.github.com>
2024-06-06 21:07:21 +0300

    adding route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,10 @@
 const express=require('express');
-const {updateSomeRecords,getAll,getPaginate,deleteRecords,getNoteVolumeRecords}=require('../controller/controller.js');
+const {updateSomeRecords,getAll,getPaginate,deleteRecord,getNoteVolumeRecords}=require('../controller/controller.js');
 const router=express.Router();
 router.put('/update/:id',updateSomeRecords);
 router.get('/',getAll);
 router.get('/paginate',getPaginate);
-router.delete('/delete/:id',deleteRecords);
+router.delete('/delete/:id',deleteRecord);
 //testing routes
 router.get('/test/note',getNoteVolumeRecords);
 module.exports=router;
