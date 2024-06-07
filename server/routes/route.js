7585a8c3ba2540d56c569dbfeb8000f5462b3dac
mohmmad ayesh <73184801+mohmmadAyesh@users.noreply.github.com>
2024-06-07 14:53:13 +0300

    adding  routes for filtering also explain it with comments

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,28 @@
 const express=require('express');
-const {updateSomeRecords,getAll,getPaginate,deleteRecord,getNoteVolumeRecords}=require('../controller/controller.js');
+const {updateRecord,getAll,getPaginate,deleteRecord,getNoteVolumeRecords,
+getRecordsFilteredByCoinPair,
+getRecordsFilteredByNote,
+getRecordsFilteredByPrice,
+getRecordsFilteredByVolume
+
+}=require('../controller/controller.js');
 const router=express.Router();
-router.put('/update/:id',updateSomeRecords);
+// updating one record by Id
+router.put('/update/:id',updateRecord);
+// retrieve all records from joined tables
 router.get('/',getAll);
+// retrieve all data paginated by page number
 router.get('/paginate',getPaginate);
+// delete record
 router.delete('/delete/:id',deleteRecord);
+// filtering by coin_pair that starts with the parameter
+router.get('/filter/coin_pair',getRecordsFilteredByCoinPair);
+// filtering by price 
+router.get('/filter/price',getRecordsFilteredByPrice);
+// filtering by note that starts with the parameter
+router.get('/filter/note',getRecordsFilteredByNote);
+// filtering by volume
+router.get('/filter/volume',getRecordsFilteredByVolume);
 //testing routes
 router.get('/test/note',getNoteVolumeRecords);
 module.exports=router;
