566f1a3a39df9968df5d81a3ce682512880ed1f1
mohmmad ayesh <73184801+mohmmadAyesh@users.noreply.github.com>
2024-06-18 20:52:32 +0300

    adding custom error middleware to app.js and then use it

--- server/app.js
+++ server/app.js
@@ -1,14 +1,17 @@
 const express=require('express');
 const router=require('./routes/route.js');
 const cors=require('cors');
+const handleErrors=require('./middlewares/handleErrors.js');
 const app=express();
 app.use(cors())
 app.use(express.urlencoded({ extended:false}));
 app.use(express.json());
 app.use('/api',router);
+app.use(handleErrors);
 async function startServer(){
     try{
          app.listen(5000,()=>{
+            console.log(handleErrors);
             console.log('App is listening on port 5000');
             });
      }catch(e){
